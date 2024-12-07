e1f46429129cb33da3ae054888cc98c378811b47
AIguibin <aiguibin@163.com>
2024-12-07 15:09:29 +0800

    add postcss-pxtorem

--- .postcssrc.js
+++ .postcssrc.js
@@ -5,6 +5,20 @@ module.exports = {
     "postcss-import": {},
     "postcss-url": {},
     // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {}
+    "autoprefixer": {},
+    "postcss-pxtorem": {
+      // 设计稿上的某个像素值作为1rem的大小
+      rootValue: 16,
+      // 需要转换的属性，'*'表示所有属性
+      propList: ['*'],
+      // 忽略的选择器
+      selectorBlackList: [],
+      // 是否直接替换原有单位
+      replace: true,
+      // 是否允许在媒体查询中转换
+      mediaQuery: false,
+      // 最小转换数值
+      minPixelValue: 0
+    }
   }
 }
